a268efe9be50c7167039210af6112bf74d53973e
viqueen <hasnae@viqueen.org>
2023-09-04 07:21:04 +0100

    welcome map with string key value pairs support

--- src/jdk.compiler/share/classes/com/sun/tools/javac/parser/JavacParser.java
+++ src/jdk.compiler/share/classes/com/sun/tools/javac/parser/JavacParser.java
@@ -2707,13 +2707,23 @@ public class JavacParser implements Parser {
      */
     public JCExpression variableInitializer(JCExpression type) {
         if (token.kind == LBRACE) {
-            if (type instanceof JCIdent && ((JCIdent) type).name.equals(names.fromString("Map"))) {
+            if (isMapType(type)) {
                 return mapInitializer(token.pos);
             } else {
                 return arrayInitializer(token.pos, null);
             }
         }
         return parseExpression();
+    }
+
+    private boolean isMapType(JCExpression type) {
+        final Object localType;
+        if (type instanceof JCTypeApply) {
+            localType = ((JCTypeApply) type).getType();
+        } else {
+            localType = type;
+        }
+        return (localType instanceof JCIdent && ((JCIdent) localType).name.equals(names.fromString("Map")));
     }
 
     private JCExpression mapInitializer(int pos) {
@@ -2724,8 +2734,11 @@ public class JavacParser implements Parser {
     private List<JCExpression> mapEntries(int pos) {
         accept(LBRACE);
         ListBuffer<JCExpression> entries = new ListBuffer<>();
-        if (token.kind != RBRACE) {
-            entries.append(mapEntry(pos));
+        while (token.kind != RBRACE) {
+            entries.append(mapEntry(token.pos));
+            if (token.kind != RBRACE) {
+                accept(COMMA);
+            }
         }
         accept(RBRACE);
         return entries.toList();
